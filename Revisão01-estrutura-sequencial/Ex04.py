0ea0115aeb1c6fee6e8e2b24168630bba459327b
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2023-08-08 21:22:48 -0300

    exercicio 4

---
+++ Revisão01-estrutura-sequencial/Ex04.py
@@ -0,0 +1,11 @@
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+notas= [0,0,0,0];
+media = 0;
+print('Insira as suas 4 notas bimestrais');
+
+for nota in range(0,4):
+    notas[nota] = float(input(f'Insira a sua nota no {nota+1} bimestre:'));
+    media = notas[nota] + media;
+
+print(media/4)
+
