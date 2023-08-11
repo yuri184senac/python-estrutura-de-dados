b93ace2881ffec0186f1ef3663936fa3fb071c7b
yuri1709 <yuriconder184@gmail.com>
2023-08-11 16:47:36 -0300

    verfificar se e numero inteiro

--- Revisão01-estrutura-sequencial/Ex11.py
+++ Revisão01-estrutura-sequencial/Ex11.py
@@ -2,4 +2,8 @@
 # o produto do dobro do primeiro com metade do segundo .
 # a soma do triplo do primeiro com o terceiro.
 # o terceiro elevado ao cubo.
+print('PROGRAMA QUE PEDE 2 NÚMEROS INTEIRO E UM NÚMERO REAL');
 
+def isNumInt(num):
+  
+  
