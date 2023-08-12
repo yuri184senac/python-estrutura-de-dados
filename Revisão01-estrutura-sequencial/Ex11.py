96ed12b455cb484b32a3cbff921ad778499499c2
yuri1709 <yuriconder184@gmail.com>
2023-08-12 07:40:14 -0300

    Ex11

--- Revisão01-estrutura-sequencial/Ex11.py
+++ Revisão01-estrutura-sequencial/Ex11.py
@@ -3,9 +3,14 @@
 # a soma do triplo do primeiro com o terceiro.
 # o terceiro elevado ao cubo.
 print('PROGRAMA QUE PEDE 2 NÚMEROS INTEIRO E UM NÚMERO REAL');
-n = float(input('Insira um número REAL'));
+n = float(input('Insira um número REAL: '));
 n2 = int(input('Insira um número inteiro:'));
 n3 = int(input('Insira outro número inteiro:'));
 
-produto = 2*(n*n2/2);
+produto = (2*n*n2/2);
+soma = 3*n + n3;
+
+print(f'produto: {produto}');
+print(f'soma: {soma}');
+print(f'{n3}^3={n3**3}');
 
