75ba918f3696f449b5fcee49ed13a1fc3c6d6869
rwauygeoagogya <36129382023.2N@senacrjedu.local>
2023-09-05 18:45:54 -0300

    estrutura de dados

---
+++ estrutura-de-dados-aula/lista/cobaia.py
@@ -0,0 +1,13 @@
+animals = ['gato', 'tatu', 'rato', 'pato'];
+
+print( animals[0])
+print( animals[1])
+print( animals[2])
+print( animals[3])
+
+
+notas = [0, 10.0, 7.0, 7.0, 8.0, 8.0]
+
+notas.insert(1, 500)
+
+print(notas)
