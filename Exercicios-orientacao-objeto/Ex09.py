214c14421c6d4b52ffa733058c7ddda3909a47cd
yuri Roliz <yuriconder184@gmail.com>
2023-09-10 09:56:16 -0300

    ex09

--- Exercicios-orientacao-objeto/Ex09.py
+++ Exercicios-orientacao-objeto/Ex09.py
@@ -4,17 +4,32 @@ class Ponto:
     self.x = x;
     self.y = y;
 
-  def get_valores(self):
-      return [self.x, self.y];
+  def get_valorX(self):
+    return self.x;
 
+  def get_valorY(self):
+    return self.y
 
-class Retangulo:
+class Retangulo():
   
   def __init__(self, largura, altura):
-    self.largura = largura;
-    self.altura = altura;
+    self.largura = largura
+    self.altura = altura
 
-  def get_centroDoRetangulo(self):
-    self.
+  def get_centro(self):
+    return f'x:{self.largura/2}, y:{self.altura/2}'
+    
+  
+vert1 = Ponto(0,0)
+vert2 = Ponto(0,6)
+vert3 = Ponto(8,6)
+vert4 = Ponto(8,0)
+
+altura = vert2.get_valorY() - vert1.get_valorY()
+largura = vert4.get_valorX() - vert1.get_valorX()
+
+retangulo1 = Retangulo(largura, altura)
+
+print(retangulo1.get_centro())
 
   
