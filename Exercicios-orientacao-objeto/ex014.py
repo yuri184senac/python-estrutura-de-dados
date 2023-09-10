348e9a03ea58c4f37236c61c091de2506e21022d
yuri Roliz <yuriconder184@gmail.com>
2023-09-10 19:14:04 -0300

    exercicio 14

---
+++ Exercicios-orientacao-objeto/ex014.py
@@ -0,0 +1,36 @@
+class Funcionario:
+    def __init__(self, nome, salario):
+        self._nome = nome
+        self.__salario = salario
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.getter
+    def nome(self):
+        return self._nome
+
+    @property
+    def salario(self):
+        return self.__salario
+    
+    @salario.getter
+    def salario(self):
+        return self.__salario
+
+    @salario.setter
+    def salario(self, novoSalario):
+        self.__salario = novoSalario
+    
+    def aumentarSalario(self, percentualAumento):
+        self.__salario += self.__salario * percentualAumento/100 
+    
+p1 = Funcionario('Yuri', 1000)
+p1.__salario = 200 #Professor, repare que não conseguimos alterar o valor do salário diretamente
+print('Nome:', p1.nome)
+print('Salario:', p1.salario)
+
+p1.aumentarSalario(10)
+print('Salario:', p1.salario)
+
