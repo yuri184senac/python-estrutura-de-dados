98ca878901aec8555747555cd1f5a09d250bd570
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2023-09-26 19:57:07 -0300

    pega ae

---
+++ estrutura-de-dados-aula/pilha/pilha-com-lista-builtin.py
@@ -0,0 +1,16 @@
+#LISTA builtin é quando temos várias  funçõe do próprio python incorap
+pilha = [1, 1, 2, 3, 5]
+print('pilha:', pilha)
+
+pilha.append(8)
+print('Inserindo um elemento:', pilha)
+
+pilha.append(13)
+print('Inserindo outro elemento: ', pilha)
+
+pilha.pop()
+print('Removendo um elemento: ', pilha)
+
+pilha.pop()
+print("Removendo outro elemento:", pilha)
+
