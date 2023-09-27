234a9485c2647cf7034172669efed2fc7881a5d2
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2023-09-26 21:49:28 -0300

    consertar o Node

--- estrutura-de-dados-aula/pilha/pilha-encadeadas.py
+++ estrutura-de-dados-aula/pilha/pilha-encadeadas.py
@@ -14,5 +14,26 @@ class Pilha:
     def __init__(self):
         self.topo = None
         
+    def insere(self, novo_dado):
+        #Cria um novo nodo com o dado a ser armazenado.
+        novo_nodo = Nodo(novo_dado)
+        
+        # Faz com que o novo nodo seja o topo da pilha
+        novo_nodo.anterior = self.topo
+        
+        #Faz com que a cabeça da lista referencia o novo nodo.
+        self.topo = novo_nodo
+        pass
+    
     def __repr__(self):
         return "[" + str(self.topo) + "]"
+    
+
+#INICIO
+
+
+stack = Pilha() #A pilha começa vazia, o topo=None. Ninguém está no topo quando a pilha é inicializada
+stack.insere(5) #O que é inserido na pilha é o nodo. O nodo armazena um valor e um endereço de memória
+stack.insere(6)
+stack.insere(7)
+print(stack)
