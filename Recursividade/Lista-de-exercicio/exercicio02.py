5e49699f51966e7cbecd4349aef83edd99f3e12b
yuri1709 <yuriconder184@gmail.com>
2023-09-28 17:23:33 -0300

    fatorial

--- Recursividade/Lista-de-exercicio/exercicio02.py
+++ Recursividade/Lista-de-exercicio/exercicio02.py
@@ -1,9 +1,12 @@
 #Faça uma função recursiva que calcule e retorne o N-ésimo termo da sequência Fibonacci.
 # Alguns números desta sequência são: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89...
 
-def calculaFibonacci(posicao):
-    if (posicao == ):
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n-1) + fibonacci(n-2)
 
-    return calculaFibonacci(posicao)
 
-
+print(fibonacci(50))
+   
+   
