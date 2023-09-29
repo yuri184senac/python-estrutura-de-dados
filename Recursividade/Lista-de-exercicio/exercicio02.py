3b18ad0cedea0e8b25c16d928dbbf1e731453299
yuri1709 <yuriconder184@gmail.com>
2023-09-28 22:58:37 -0300

    exercicio 6

--- Recursividade/Lista-de-exercicio/exercicio02.py
+++ Recursividade/Lista-de-exercicio/exercicio02.py
@@ -7,6 +7,6 @@ def fibonacci(n):
     return fibonacci(n-1) + fibonacci(n-2)
 
 
-print(fibonacci(50))
+print(fibonacci(10))
    
    
---
+++ Recursividade/Lista-de-exercicio/exercicio06.py
@@ -0,0 +1,14 @@
+#Crie um programa em python, que contenha uma função recursiva que receba dois inteiros positivos k e n e calcule k^n . Utilize apenas multiplicações. O programa principal deve solicitar ao usuário os valores de k e n e imprimir o resultado da chamada da função. 
+
+# k = int(input('Insira valor de k: '));
+# n = int(input('Insira valor de n: '));
+
+
+def potencia(k,n):
+    if (n==1):
+        return k
+    return potencia(k,n-1)*k
+
+print(potencia(2,2))
+
+
