e04dcaa85e6395c72521097b097d2ef3d17ad02c
yuri1709 <yuriconder184@gmail.com>
2023-10-02 15:42:02 -0300

    ex8

--- Recursividade/Lista-de-exercicio/exercicio03.py
+++ Recursividade/Lista-de-exercicio/exercicio03.py
@@ -1,9 +1,16 @@
 #3) Faça uma função recursiva que permita inverter um número inteiro N. Ex: 123 - 321 
 
-def inverte(num, aux=0):
-      
-  resultado = num // 100;
-  resto = num % 100
+def inverte(num, aux=0):   
+  resultado = num // 100; #1
+  resto = num % 100 #23
+  resultado2 = resto // 10
+  resto2 = resto % 10
+  final  = str(resto2) + str(resultado2)  + str(resultado);
+  return final
+print(inverte(123))
+
+def inverte2(num, divisor):
+   if 
 
 
 
--- Recursividade/Lista-de-exercicio/exercicio08.py
+++ Recursividade/Lista-de-exercicio/exercicio08.py
@@ -1,2 +1,13 @@
 #  O máximo divisor comum dos inteiros x e y é o maior inteiro que é divisível por x e y. Escreva uma função recursiva mdc em python, que retorna o máximo divisor comum de x e y. O mdc de x e y é definido como segue: se y é igual a 0, então mdc(x,y) é x; caso contrário, mdc(x,y) é mdc (y, x%y), onde % é o operador resto. 
 
+def mdc(x, y):
+    if (x < y):
+        return mdc(y, x);
+    
+    else :
+        if (x % y == 0):
+            return y;
+        else:
+            return mdc(y, x%y);
+    
+print(mdc(8,36))
