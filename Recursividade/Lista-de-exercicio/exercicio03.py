87b960d76d88cda8908d8c14356e25c6b1fce012
yuri1709 <yuriconder184@gmail.com>
2023-10-02 16:54:30 -0300

    ex03

--- Recursividade/Lista-de-exercicio/exercicio03.py
+++ Recursividade/Lista-de-exercicio/exercicio03.py
@@ -1,16 +1,13 @@
 #3) Faça uma função recursiva que permita inverter um número inteiro N. Ex: 123 - 321 
 
-def inverte(num, aux=0):   
+def inverte(num, aux=0) -> int:   
   resultado = num // 100; #1
   resto = num % 100 #23
   resultado2 = resto // 10
   resto2 = resto % 10
   final  = str(resto2) + str(resultado2)  + str(resultado);
-  return final
+  return int(final)
+
 print(inverte(123))
-
-def inverte2(num, divisor):
-   if 
-
 
 
--- Recursividade/Lista-de-exercicio/tempCodeRunnerFile.py
+++ Recursividade/Lista-de-exercicio/tempCodeRunnerFile.py
@@ -1,1 +1,12 @@
-num
+def inverte2(num, divisor):
+#   resultado = num // divisor;
+#   if divisor == 10:
+#     resultado = resto // 10
+
+#     return num
+#   else:  
+#     resto = num % divisor;
+
+#     return inverte2(num, divisor//10)
+
+# print(inverte2(123, 100))
