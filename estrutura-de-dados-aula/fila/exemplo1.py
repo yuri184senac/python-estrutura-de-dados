9b2ae105b7551a0d4d4ed28e2df660eae4f35049
ADS 2023.2N <36129382023.2N@senacrjedu.local>
2023-10-03 21:22:16 -0300

    fila

--- estrutura-de-dados-aula/fila/exemplo1.py
+++ estrutura-de-dados-aula/fila/exemplo1.py
@@ -24,9 +24,25 @@ class Fila:
         
         #Insere em uma fila vazia
         if self.primeiro == None:
-            
+            self.primeiro = novo_nodo
+            self.ultimo = novo_nodo
+        else:
+            #faz com que o novo nodo seja o último da fila.
+            self.ultimo.proximo = novo_nodo
+            #faz com que o ultimo da filda referencie o novo nodo.
+            self.ultimo = novo_nodo
+    
+    def remove(self):
+        """Remove o último elemento da fila."""
+        
+        assert self.primeiro != None, "Impossível remover elemento de fila vazia." #esse assert é pra tratamento de erro
+        self.primeiro = self.primeiro.proximo#esse é o nodo que está atrás do primeiro nodo
+        
+        if self.primeiro == None:
+            self.ultimo = None  
+    
     def __repr__(self):
         return "["+ str(self.primeiro) + "]"
-    
+
     
     
